1fd2c41fba54dc09aa07b0a10d62e81d416e70bc
Rowsen <bayramovw0@gmail.com>
2023-09-17 14:42:37 +0500

    changed db to postgresql

--- wes_scan/settings/base.py
+++ wes_scan/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -68,8 +69,12 @@ WSGI_APPLICATION = 'wes_scan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("POSTGRES_NAME", 'wes_scan'),
+        'USER': os.environ.get("POSTGRES_USER", 'postgres'),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_NAME", 'localhost'),
+        'PORT': os.environ.get("POSTGRES_NAME", '8000'),
     }
 }
 
