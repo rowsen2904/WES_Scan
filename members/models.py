62b6b067514e2528048dcacd5cff8a9a8c8f4b27
Rowsen <bayramovw0@gmail.com>
2023-10-02 08:46:28 +0500

    added slug field, squashed migrations

--- members/models.py
+++ members/models.py
@@ -1,4 +1,7 @@
+from uuid import uuid4
+
 from django.db import models
+from django.utils.text import slugify
 
 
 class Specialization(models.Model):
@@ -28,6 +31,13 @@ class Member(models.Model):
     specializations = models.ManyToManyField(Specialization, related_name='member')
     dates = models.ManyToManyField(Date, related_name="activity", blank=True)
     image = models.ImageField(upload_to="members", blank=True, null=True)
+    slug = models.SlugField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.id}. {self.fullname}"
+
+    def save(self, *args, **kwargs) -> None:
+        if self.id is None:
+            self.slug = slugify(str(uuid4()))
+
+        super().save(*args, **kwargs)
